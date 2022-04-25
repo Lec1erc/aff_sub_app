64446236d890fe96b774d3c079c8963711cf5abf
slash <slash526@gmail.com>
2022-04-25 15:44:52 +0300

    Initial commit

--- aff_report/index.py
+++ aff_report/index.py
@@ -11,7 +11,7 @@ accounts = "data/accounts.txt"
 app = Flask(__name__)
 CORS(app)
 
-print("New poi")
+print("New ctest")
 print("Test branch")
 print("asdasdasd")
 @app.route('/sub_report', methods=(['POST']))
